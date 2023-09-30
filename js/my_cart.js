a837d736f2dffbc9565a096ed38e6e65370728f6
Uma Roy <umaroy.developer@gmail.com>
2023-09-30 12:51:39 +0530

    first commit

---
+++ js/my_cart.js
@@ -0,0 +1,58 @@
+const ctx = document.getElementById('myChart').getContext('2d');
+const earning = document.getElementById('earning').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'polarArea',
+    data: {
+        labels: ['Facebook', 'YouTube', 'Amazon'],
+        datasets: [{
+            label: 'Traffic Source',
+            data: [12000, 19000, 30000],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+        }]
+    },
+    options: {
+        responsive: true,
+    }
+});
+
+const myChart1 = new Chart(earning, {
+    type: 'bar',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        datasets: [{
+            label: 'Earning',
+            data: [1200, 1090, 3000, 5400, 1220, 3600, 6800, 4578, 2566, 8545, 4587, 9000],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+    }
+});
+// Chart.js CDN using in it.
